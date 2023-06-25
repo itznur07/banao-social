734eedb47c42e99d7c844dd07c3005ca284efdc3
Md Nuruddin <nuruddinalways99@gmail.com>
2023-06-25 11:27:15 +0600

    Enveronment setup

---
+++ src/Provider/AuthContext.jsx
@@ -0,0 +1,82 @@
+// import {
+//   GoogleAuthProvider,
+//   createUserWithEmailAndPassword,
+//   getAuth,
+//   onAuthStateChanged,
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   signOut,
+//   updateProfile,
+// } from "firebase/auth";
+import React, { createContext, useState } from "react";
+// import app from "../Firebase/firebase.config";
+
+export const AuthContext = createContext();
+// const auth = getAuth(app);
+
+const AuthPorvider = ({ children }) => {
+  const [user, setUser] = useState({
+    name: "asif",
+  });
+  const [loading, setLoading] = useState(true);
+  // const [dbUsers, setDbUsers] = useState([]);
+
+  /** Database Users */
+
+  /** conmmunication with firebase */
+  // const createUserWithEmailPassword = (email, password) => {
+  //   setLoading();
+  //   return createUserWithEmailAndPassword(auth, email, password);
+  // };
+
+  // const signInWithGoogle = () => {
+  //   setLoading();
+  //   return signInWithPopup(auth, new GoogleAuthProvider());
+  // };
+
+  // const logInUserWithEmailPassword = (email, password) => {
+  //   setLoading();
+  //   return signInWithEmailAndPassword(auth, email, password);
+  // };
+  // const logOut = () => {
+  //   setLoading();
+  //   return signOut(auth);
+  // };
+
+  // const profileUpdate = (name, photo) => {
+  //   setLoading();
+  //   return updateProfile(auth.currentUser, {
+  //     displayName: name,
+  //     photoURL: photo,
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   const unsubscibe = onAuthStateChanged(auth, (currentUser) => {
+  //     setUser(currentUser);
+  //     console.log(currentUser);
+  //     setLoading(false);
+  //   });
+
+  //   return () => {
+  //     unsubscibe();
+  //   };
+  // }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    // dbUsers,
+    // createUserWithEmailPassword,
+    // logInUserWithEmailPassword,
+    // logOut,
+    // profileUpdate,
+    // signInWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthPorvider;
